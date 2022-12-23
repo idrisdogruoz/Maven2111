8f61ee00a1c27b37f720807bf34f476d3555ca31
idris <idrisdogruoz@gmail.com>
2022-12-23 18:22:35 +0300

    yahoo task

--- src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -1,7 +1,25 @@
 package com.cydeo.tests.day1_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1_YahooTitleVerification {
     public static void main(String[] args) {
+      //  TC #1: Yahoo Title Verification
+      // do set up for browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //1. OPEN  Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        //2. Go to https://www.yahoo.com
+      driver.get("https://www.yahoo.com");
+
+        //3. Verify title:
+
+
+        //Expected: Yahoo
 
 
 
